79e51b633b085a12aa5d2cfa78a45d1c44e6f5af
Chimezie Okeke <ChimezieOkeke@wmpenn.edu>
2024-03-18 18:40:43 -0400

    assignment one

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import ItemList from './components/ItemList';
 
 function App() {
+  const [items, setItems] = useState([
+    { id: 1, name: 'Chimezie Okeke LinkedIn' },
+    { id: 2, name: 'Chimezie Okeke Basketball Profile' },
+    { id: 3, name: 'Chimezie Okeke Programming Channel' }
+  ]);
+
+  const handleItemClick = (item) => {
+    console.log('Item clicked in App:', item);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>This is assignment #1</h1>
       </header>
+      <div className="item-list-container">
+        <ItemList items={items} onItemClick={handleItemClick} />
+      </div>
     </div>
-  );
+  );DOMException
 }
 
 export default App;
